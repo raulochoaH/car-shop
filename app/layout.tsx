e09917d992a93dd1a4b9aa9aa82b4ffac534932e
Raul Ochoa <raulochoa.dev@gmail.com>
2023-12-12 12:11:21 -0400

    feat: push project to GitHub

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Navbar, Footer} from '@/components'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Car Hub',
+  description: 'Discover the best cars in the world',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar/>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import CustomBottom from './CustomBotton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+export {
+    Hero,
+    CustomBottom,
+    Navbar,
+    Footer
+}
