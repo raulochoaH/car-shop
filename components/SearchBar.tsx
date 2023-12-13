cb10a2202b9cc6ad93553cbd2a6561c5ec09929f
Raul Ochoa <raulochoa.dev@gmail.com>
2023-12-12 21:10:17 -0400

    feat: add footer, navbar and start with the car catalogue

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import {SearchManufacturer} from "./"
+
+const SearchBar = () => {
+ 
+
+  return (
+    <form className="searchbar" >
+      <div className="searbar__item">
+        <SearchManufacturer/>
+      </div>
+
+    </form>
+  )
+}
+
+export default SearchBar
--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,16 @@ import Hero from './Hero';
 import CustomBottom from './CustomBotton'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer'
+
 export {
     Hero,
     CustomBottom,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
